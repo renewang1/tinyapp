0eacd60f16a017c81582d4dbf7c2ac82b73fa7b1
Rene Wang <rene.wang1@hotmail.com>
2020-12-08 22:33:23 +0000

    added delete button and requisite post routing

--- express_server.js
+++ express_server.js
@@ -47,6 +47,12 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect(`/urls`);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
