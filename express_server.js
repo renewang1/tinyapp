f3a4e3946d88cf244fe574bb491a29b9aaf5cc2c
Rene Wang <rene.wang1@hotmail.com>
2020-12-08 21:33:42 +0000

    created generateRandomString function

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
@@ -22,6 +26,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
